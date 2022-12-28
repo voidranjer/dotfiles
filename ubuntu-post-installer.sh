fbd12cfa63a94e77ce930f9a063f325039a0e744
RavenJS <logicism81@gmail.com>
2022-12-28 01:35:59 +0000

    Changed ~/.local to /usr/local

--- ubuntu-post-installer.sh
+++ ubuntu-post-installer.sh
@@ -6,8 +6,13 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-# Create ~/.local/bin directory if it doesn't already exist
-mkdir -p ~/.local/bin
+# Variables
+BIN_PATH="/usr/local/bin"
+BG_PATH="/usr/local/share/backgrounds"
+
+# Create directories if they doesn't already exist 
+sudo mkdir -p $BIN_PATH
+sudo mkdir -p $BG_PATH
 
 
 # Add PPAs
@@ -31,16 +36,16 @@ sudo apt-get -qq install firefox thunar gedit alacritty
 
 # Install i3 autotiling (.local/bin/autotiling)
 sudo apt-get -qq install python3-i3ipc
-curl -s -o ~/.local/bin/autotiling -L "https://raw.githubusercontent.com/nwg-piotr/autotiling/master/autotiling/main.py"
-sudo chmod +x ~/.local/bin/autotiling
+sudo curl -s -o $BIN_PATH/autotiling -L "https://raw.githubusercontent.com/nwg-piotr/autotiling/master/autotiling/main.py"
+sudo chmod +x $BIN_PATH/autotiling
 
 # Install i3status-title-on-bar
-curl -s -o ~/.local/bin/i3status-title-on-bar -L "https://github.com/rholder/i3status-title-on-bar/releases/latest/download/i3status-title-on-bar-linux_amd64"
-sudo chmod +x ~/.local/bin/i3status-title-on-bar
+sudo curl -s -o $BIN_PATH/i3status-title-on-bar -L "https://github.com/rholder/i3status-title-on-bar/releases/latest/download/i3status-title-on-bar-linux_amd64"
+sudo chmod +x $BIN_PATH/i3status-title-on-bar
 
 # Install pfetch (used by ~/.config/fish/config.fish)
-curl -s -o ~/.local/bin/pfetch -L "https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch"
-sudo chmod +x ~/.local/bin/pfetch
+sudo curl -s -o $BIN_PATH/pfetch -L "https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch"
+sudo chmod +x $BIN_PATH/pfetch
 
 # Install bumblebee-status (for i3 bar)
 sudo apt-get -qq install fonts-font-awesome fonts-powerline python3-pip
@@ -53,6 +58,7 @@ echo "y" | sudo ufw enable
 
 # Wallpaper Setup
 echo "Setting up wallpaper for the first time"
-nitrogen --set-zoom-fill ~/Pictures/Wallpapers/wallpaper.png
+> /dev/null
+nitrogen --set-zoom-fill $BG_PATH/wallpaper.png
 
 echo "Completed!"
