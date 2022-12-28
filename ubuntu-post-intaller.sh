3c70ff5ed319706d00959dfef5cf48b48b19aa06
James <logicism81@gmail.com>
2022-12-28 00:27:01 +0000

    Update ubuntu-post-intaller.sh

--- ubuntu-post-intaller.sh
+++ ubuntu-post-intaller.sh
@@ -1,67 +1,51 @@
 #!/bin/bash
 
-# Functions
-spin(){
-  while [ 1 ]
-  do 
-    for i in ${spinner[@]}; 
-    do 
-      echo -ne "\r$i";
-      sleep 0.2;
-    done;
-  done
-}
+# Check sudo privileges
+if [ "$EUID" -ne 0 ]
+  then echo "This script has to be run with root! (Hint: Use 'sudo' when running this script)"
+  exit
+fi
 
-# Spinner animation
-spinner=( Ooooo oOooo ooOoo oooOo ooooO oooOo ooOoo oOooo);
 
 # Add PPAs
-echo "Adding additional PPAs"
-spin &
-pid=$!
-yes "" | sudo add-apt-repository ppa:regolith-linux/release > /dev/null # i3-gaps
-yes "" | sudo add-apt-repository ppa:aslatter/ppa > /dev/null # alacritty
-kill $pid 
+echo "Adding additional PPAs..."
+sudo add-apt-repository -y ppa:regolith-linux/release > /dev/null # i3-gaps
+sudo add-apt-repository -y ppa:aslatter/ppa > /dev/null # alacritty
 
 # Updates
-echo "Running `apt update` and `apt upgrade`"
-spin &
-pid=$!
-sudo apt -y update && sudo apt -y upgrade > /dev/null
-kill $pid 
+echo "Running 'apt-get update, upgrade, and autoremove'"
+sudo apt-get -qq update
+sudo apt-get -qq upgrade
+sudo apt-get -qq autoremove
 
-echo "Installing packages"
-spin &
-pid=$!
+echo "Installing packages..."
 
 # Install packages (required by ~/.config/i3/config)
-sudo apt -y install htop rofi fish ranger nitrogen picom redshift-gtk volumeicon-alsa arandr i3-gaps > /dev/null
+sudo apt-get -qq install htop rofi fish ranger nitrogen picom redshift-gtk volumeicon-alsa arandr i3-gaps
 
 # Install other useful packages
-sudo apt -y install firefox thunar gedit alacritty vscode > /dev/null
+sudo apt-get -qq install firefox thunar gedit alacritty
 
 # Install i3 autotiling (.local/bin/autotiling)
-sudo apt -y install python3-i3ipc > /dev/null
-curl -o ~/.local/bin/autotiling -L "https://raw.githubusercontent.com/nwg-piotr/autotiling/master/autotiling/main.py" > /dev/null
+sudo apt-get -qq install python3-i3ipc
+curl -s -o ~/.local/bin/autotiling -L "https://raw.githubusercontent.com/nwg-piotr/autotiling/master/autotiling/main.py"
 sudo chmod +x ~/.local/bin/autotiling
 
 # Install i3status-title-on-bar
-curl -o ~/.local/bin/i3status-title-on-bar -L "https://github.com/rholder/i3status-title-on-bar/releases/latest/download/i3status-title-on-bar-linux_amd64" > /dev/null
+curl -s -o ~/.local/bin/i3status-title-on-bar -L "https://github.com/rholder/i3status-title-on-bar/releases/latest/download/i3status-title-on-bar-linux_amd64"
 sudo chmod +x ~/.local/bin/i3status-title-on-bar
 
 # Install pfetch (used by ~/.config/fish/config.fish)
-curl -o ~/.local/bin/pfetch -L "https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch" > /dev/null
+curl -s -o ~/.local/bin/pfetch -L "https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch"
 sudo chmod +x ~/.local/bin/pfetch
 
 # Install bumblebee-status (for i3 bar)
-sudo apt -y install fonts-font-awesome fonts-powerline python3-pip
-pip3 install --user bumblebee-status
-
-kill $pid
+sudo apt-get -qq install fonts-font-awesome fonts-powerline python3-pip
+pip3 install --user --quiet bumblebee-status
 
 # Enable Firewall
 echo "Enabling Firewall"
-sudo apt -y install gufw
+sudo apt-get -qq install gufw
 echo "y" | sudo ufw enable
 
 # Wallpaper Setup
