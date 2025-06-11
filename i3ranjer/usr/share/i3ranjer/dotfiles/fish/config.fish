c8b5ee5da4879c93835613de9a3e0745cac831a8
James Yap <james.yap@ouster.io>
2025-06-11 10:01:40 -0400

    [feat] ranger-cd in fish + tmux conf for lazynvim

--- i3ranjer/usr/share/i3ranjer/dotfiles/fish/config.fish
+++ i3ranjer/usr/share/i3ranjer/dotfiles/fish/config.fish
@@ -2,3 +2,17 @@
 alias ghcmd="gh copilot suggest"
 alias ghask="gh copilot explain"
 alias lg="lazygit"
+
+# ranger-cd
+function fm
+    set tempfile /tmp/chosendir
+    ranger --choosedir=$tempfile (pwd)
+
+    if test -f $tempfile
+        if [ (cat $tempfile) != (pwd) ]
+            cd (cat $tempfile)
+        end
+    end
+
+    rm -f $tempfile
+end
