f8e8b2fb76cbce8dffe4f87c03780f9002ad5080
James Yap <contact@jamesyap.org>
2025-07-24 21:25:44 -0400

    feat: forever loop to chat.fish

--- stow/fish/.config/fish/functions/chat.fish
+++ stow/fish/.config/fish/functions/chat.fish
@@ -1,15 +1,17 @@
 function chat
-    set tmp (mktemp)
-    nvim $tmp +startinsert
+    while true
+        set tmp (mktemp)
+        nvim $tmp +startinsert
 
-    # Check if the file is empty
-    if test (wc -c < $tmp | string trim) -eq 0
+        # Check if the file is empty
+        if test (wc -c < $tmp | string trim) -eq 0
+            rm $tmp
+            return
+        end
+
+        cat $tmp | llm -m github_copilot/gpt-4.1 $argv
+        clear
+        llm logs -r | glow
         rm $tmp
-        return
     end
-
-    cat $tmp | llm -m github_copilot/gpt-4.1 $argv
-    clear
-    llm logs -r | glow
-    rm $tmp
 end
