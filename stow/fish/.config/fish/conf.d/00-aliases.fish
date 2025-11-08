76aa95b87ec1f52f726cdfe42413b380665df679
James Yap <contact@jamesyap.org>
2025-11-08 00:28:20 -0500

    feat: llm aliases, and vimrc

--- stow/fish/.config/fish/conf.d/00-aliases.fish
+++ stow/fish/.config/fish/conf.d/00-aliases.fish
@@ -3,3 +3,8 @@ alias lah="lsd -lah"
 # git
 alias lg="lazygit"
 alias gst="git status"
+
+alias gpt="llm chat -m github_copilot/gpt-4.1 -s \"Provide short answers.\""
+alias gpt5="llm chat -m github_copilot/gpt-5-mini"
+alias gem="llm chat -m github_copilot/gemini-2.5-pro"
+
